2e50420fd0c0abf0a1e7dceac9d81fee27711d30
rafaeltavaares <rafaeltavsilva@gmail.com>
2024-06-06 10:30:01 -0300

    Feat: first Commit

---
+++ src/main/java/com/example/crudcomservico/CrudcomservicoApplication.java
@@ -0,0 +1,14 @@
+package com.example.crudcomservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudcomservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudcomservicoApplication.class, args);
+		System.out.println("running...");
+	}
+
+}
---
+++ src/test/java/com/example/crudcomservico/crudcomservico/CrudcomservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudcomservico.crudcomservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudcomservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
