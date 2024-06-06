57ffc79fd7585d01f20074014566b6844f8e8540
rafaeltavaares <rafaeltavsilva@gmail.com>
2024-06-06 10:31:59 -0300

    Feat: Creating Status Enum class

---
+++ src/main/java/com/example/crudcomservico/DTOs/StatusDTO.java
@@ -0,0 +1,6 @@
+package com.example.crudcomservico.DTOs;
+
+import com.example.crudcomservico.domain.Status;
+
+public record StatusDTO(Status status) {
+}
---
+++ src/main/java/com/example/crudcomservico/domain/Status.java
@@ -0,0 +1,5 @@
+package com.example.crudcomservico.domain;
+
+public enum Status {
+    PAID, PENDING
+}
